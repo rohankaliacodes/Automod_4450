b0245f0cca439159f17605a85e8588415cfa92dc
slionett <slionett@uwo.ca>
2024-12-19 19:00:19 -0500

    username display fix

--- my-app/src/client/src/pages/HomePage.js
+++ my-app/src/client/src/pages/HomePage.js
@@ -12,7 +12,7 @@ function Homepage() {
     const [engine, setEngine] = React.useState("");
     const [loggedIn, setLoggedIn] = React.useState(false);
 
-    loggedIn = sessionStorage.getItem("email") ? true : false;
+    setLoggedIn(sessionStorage.getItem("email") !== null);
 
 
     const makes = ["Toyota", "Honda"];
