ac45f5b9a983015a620d80ff01417fe46e5e4f13
Dev Panchal <90882951+DevPanchal02@users.noreply.github.com>
2025-03-16 15:21:41 -0400

    deleted redundant loginpage and added button hovers for login

--- my-app/src/client/src/pages/Register.js
+++ my-app/src/client/src/pages/Register.js
@@ -112,12 +112,12 @@ function RegisterPage() {
             className="input-field"
           />
           <button type="submit" className="sign-in-button">
-            Sign Up
+            <span>Sign Up</span>
           </button>
           <div className="separator"></div>
           <button onClick={handleGoogleSignUp} className="google-button" type="button">
             <img src={googleLogo} alt="Google Logo" />
-            Sign up with Google
+            <span>Sign up with Google</span>
           </button>
           <div className="signup-link">
             <p>
