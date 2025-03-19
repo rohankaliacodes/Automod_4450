990e1d052f376f63e40d33f324b4da31befceb79
JaskomalMattu21 <137728710+JaskomalMattu21@users.noreply.github.com>
2025-03-19 19:18:44 -0400

    fixed text in modshop

--- my-app/src/client/src/index.js
+++ my-app/src/client/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './styles/styles.css'; // Apply global styles
 import App from './App';
+import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root')); // Use createRoot
 root.render(
